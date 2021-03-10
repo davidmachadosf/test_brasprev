0ec9d7926cefb2b956e9d8a5cb3435f1c80146ea
David Machado <dfilho-git>
2021-03-09 22:54:06 -0300

    apontando para o meu github

--- src/main/java/br/com/davidmachadosf/test_brasprev/restserv/RestservApplication.java
+++ src/main/java/br/com/davidmachadosf/test_brasprev/restserv/RestservApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestservApplication {
 
+	/* primeiro deploy no docker */
 	public static void main(String[] args) {
 		SpringApplication.run(RestservApplication.class, args);
 	}
